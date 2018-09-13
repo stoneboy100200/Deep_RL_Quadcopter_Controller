9dd61df4b0c8a081d692dd86699cb58824a8b47c
seven <stoneboy100200@126.com>
2018-09-13 16:24:14 +0800

    finish all tasks

---
+++ agents/agent.py
@@ -0,0 +1,128 @@
+import numpy as np
+from actor import Actor
+from critic import Critic
+from ou_noise import OUNoise
+from memory import ReplayBuffer
+
+
+# 智能体
+class DDPG():
+    """Reinforcement Learning agent , learning using DDPG."""
+    def __init__(self, task):
+        self.task = task
+        self.state_size = task.state_size
+        self.action_size = task.action_size
+        self.action_low = task.action_low
+        self.action_high = task.action_high
+
+        # Actor (Policy) Model
+        self.actor_local = Actor(self.state_size, self.action_size, self.action_low, self.action_high)
+        self.actor_target = Actor(self.state_size, self.action_size, self.action_low, self.action_high)
+
+        # Critic (Value) Model
+        self.critic_local = Critic(self.state_size, self.action_size)
+        self.critic_target = Critic(self.state_size, self.action_size)
+
+        # Initialize target model parameters with local model parameters
+        self.critic_target.model.set_weights(self.critic_local.model.get_weights())
+        self.actor_target.model.set_weights(self.actor_local.model.get_weights())
+
+        # Noise process
+        self.exploration_mu = 0
+        self.exploration_theta = 0.08
+        self.exploration_sigma = 0.15
+        self.noise = OUNoise(self.action_size, self.exploration_mu, self.exploration_theta, self.exploration_sigma)
+
+        # Replay memory
+        self.buffer_size = 100000
+        self.batch_size = 64
+        self.memory = ReplayBuffer(self.buffer_size, self.batch_size)
+
+        # Algorithm parameters
+        self.gamma = 0.95  # discount factor 0.99
+        self.tau = 0.001  # for soft update of target parameters 0.01
+
+        # Score tracker and learning parameters
+        self.total_reward = None
+        self.count = 0
+        self.score = 0
+        self.best_score = -np.inf
+        self.last_state = None
+
+    def reset_episode(self):
+
+        self.total_reward = None
+        self.count = 0
+
+        self.noise.reset()
+        state = self.task.reset()
+        self.last_state = state
+        return state
+
+    def step(self, action, reward, next_state, done):
+
+        if self.total_reward:
+            self.total_reward += reward
+        else:
+            self.total_reward = reward
+
+        self.count += 1
+
+        # Save experience / reward
+        self.memory.add(self.last_state, action, reward, next_state, done)
+
+        # Learn, if enough samples are available in memory
+        if len(self.memory) > self.batch_size:
+            experiences = self.memory.sample()
+            self.learn(experiences)
+
+        # Roll over last state and action
+        self.last_state = next_state
+
+    def act(self, states):
+        """Returns actions for given state(s) as per current policy."""
+        states = np.reshape(states, [-1, self.state_size])
+        action = self.actor_local.model.predict(states)[0]
+        # add some noise for exploration
+        return list(action + self.noise.sample())
+
+    def learn(self, experiences):
+        """Update policy and value parameters using given batch of reward tuples."""
+
+        # Convert experience tuples to separate arrays for each element (states, actions, rewards, etc.)
+        states = np.vstack([e.state for e in experiences if e is not None])
+        actions = np.array([e.action for e in experiences if e is not None]).astype(np.float32).reshape(-1, self.action_size)
+        rewards = np.array([e.reward for e in experiences if e is not None]).astype(np.float32).reshape(-1, 1)
+        dones = np.array([e.done for e in experiences if e is not None]).astype(np.uint8).reshape(-1, 1)
+        next_states = np.vstack([e.next_state for e in experiences if e is not None])
+
+        # Get predicted actions of next-state  and Q values from target models
+        actions_next = self.actor_target.model.predict_on_batch(next_states)
+        Q_targets_next = self.critic_target.model.predict_on_batch([next_states, actions_next])
+
+        # Compute Q targets for current states and train critic model (local)
+        Q_targets = rewards + self.gamma * Q_targets_next * (1 - dones)
+        self.critic_local.model.train_on_batch(x=[states, actions], y=Q_targets)
+
+        # Train actor model (local)
+        action_gradients = np.reshape(self.critic_local.get_action_gradients([states, actions, 0]), (-1, self.action_size))
+        self.actor_local.train_fn([states, action_gradients, 1])  # custom training function
+
+        # Soft-update target models
+        self.soft_update(self.critic_local.model, self.critic_target.model)
+        self.soft_update(self.actor_local.model, self.actor_target.model)
+
+        # track best score
+        self.score = self.total_reward / float(self.count) if self.count else -np.inf
+        if self.best_score < self.score:
+            self.best_score = self.score
+
+    def soft_update(self, local_model, target_model):
+        """Soft update model parameters."""
+        local_weights = np.array(local_model.get_weights())
+        target_weights = np.array(target_model.get_weights())
+
+        assert len(local_weights) == len(target_weights), "Local and target model parameters must have the same size"
+
+        new_weights = self.tau * local_weights + (1 - self.tau) * target_weights
+        target_model.set_weights(new_weights)
